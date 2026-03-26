874c6b26d3cb550e7fc77bf070585853ac77219e
Yusuf Ulaş Yıldız <y.ulas.yildiz@gmail.com>
2026-03-26 15:05:33 +0300

    single collateral asset integrated
    single token can be collateralized

--- src/StabletokenEngine.sol
+++ src/StabletokenEngine.sol
@@ -20,11 +20,6 @@ contract StabletokenEngine is ReentrancyGuard {
         _;
     }
 
-    modifier isCollateral(address token) {
-        if (priceFeeds[token] == address(0)) revert StabletokenEngine__NotCollateral(token);
-        _;
-    }
-
     // mint()
     // burn()
     // liquidate()
@@ -34,43 +29,27 @@ contract StabletokenEngine is ReentrancyGuard {
 
     Stabletoken private immutable sbt;
 
-    mapping(address collateral => address priceFeed) private priceFeeds;
+    address private immutable priceFeed;
 
-    mapping(address user => mapping(address collateral => uint256 amount)) private deposited;
+    address private immutable collateralToken;
+
+    mapping(address user => uint256 amount) private deposited;
 
     mapping(address user => uint256 amount) private minted;
-
-    address[] private collateralTokens;
 
     // Events
     event Deposit(address indexed user, address indexed token, uint256 indexed amount);
 
     // Functions
 
-    constructor(
-        address[] memory collateralTokenAddresses,
-        address[] memory priceFeedAddresses,
-        address stabletokenAddress
-    ) {
-        if (collateralTokenAddresses.length != priceFeedAddresses.length) {
-            revert StabletokenEngine__CollateralTokenAddressesAndPriceFeedsAddressesDontMatch();
-        }
-
-        for (uint256 i = 0; i < collateralTokenAddresses.length; i++) {
-            priceFeeds[collateralTokenAddresses[i]] = priceFeedAddresses[i];
-            collateralTokens.push(collateralTokenAddresses[i]);
-        }
-
+    constructor(address _collateralToken, address _priceFeed, address stabletokenAddress) {
+        collateralToken = _collateralToken;
+        priceFeed = _priceFeed;
         sbt = Stabletoken(stabletokenAddress);
     }
 
-    function deposit(address collateralToken, uint256 amount)
-        public
-        moreThanZero(amount)
-        isCollateral(collateralToken)
-        nonReentrant
-    {
-        deposited[msg.sender][collateralToken] += amount;
+    function deposit(uint256 amount) public moreThanZero(amount) nonReentrant {
+        deposited[msg.sender] += amount;
         emit Deposit(msg.sender, collateralToken, amount);
         bool success = IERC20(collateralToken).transferFrom(msg.sender, address(this), amount);
         if (!success) revert StabletokenEngine__TransferFailed();
