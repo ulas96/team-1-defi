7ff4100f3f4bcbf9eb515d9b067a47b01ef22f91
Yusuf Ulaş Yıldız <y.ulas.yildiz@gmail.com>
2026-03-30 14:08:00 +0300

    oracle lib update
    stale prices check added

--- src/library/Oracle.sol
+++ src/library/Oracle.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.29;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library Oracle {
-    error OracleLib__StalePrice();
+    error Oracle__StalePrice();
+
+    uint256 private constant TIMEOUT = 3 hours;
 
     function getOraclePrice(AggregatorV3Interface _priceFeed)
         public
@@ -14,8 +16,8 @@ library Oracle {
         (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
             _priceFeed.latestRoundData();
 
-        if (updatedAt == 0 || answeredInRound < roundId) {
-            revert OracleLib__StalePrice();
+        if (updatedAt == 0 || answeredInRound < roundId || block.timestamp - updatedAt > TIMEOUT) {
+            revert Oracle__StalePrice();
         }
 
         return (roundId, answer, startedAt, updatedAt, answeredInRound);
